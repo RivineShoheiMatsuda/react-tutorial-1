6355bfd45c7db9a39afe8d3f8a94fcd7afe78b74
RivineShoheiMatsuda <shohei.matsuda@rivine.jp>
2022-11-21 15:05:57 +0900

    Make New Files

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { useState, useRef } from "react";
+import TodoList from "./TodoList";
+import { v4 as uuidv4 } from "uuid";
+function App() {
+  const [todos, setTodos] = useState([
+    {id: 1, name: "Todo1", completed: false},
+  ]);
+
+  const todoNameRef = useRef();
+
+  const handoleAddTodo = () => {
+    // タスクを追加する
+    const name = todoNameRef.current.value;
+    if(name === "") return;
+    setTodos((prevTodos) => {
+      return [...prevTodos, {id: uuidv4(), name: name , completed: false }];
+    });
+    todoNameRef.current.value = null;
+  };
+
+  const toggleTodo = (id) => {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.completed = !todo.completed;
+    setTodos(newTodos);
+  };
+
+  const handoleClear = () => {
+    const newTodos = todos.filter((todo) => !todo.completed);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <input type="text" ref={todoNameRef} />
+      <button onClick={handoleAddTodo}>タスクを追加</button>
+      <button onClick={handoleClear}>完了したタスクの削除</button>
+      <div>残りのタスク：{todos.filter((todo) => !todo.completed).length}</div>
+    </div>
+  );
+}
+
+export default App;
