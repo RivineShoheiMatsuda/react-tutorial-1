c388df7ad94658bcdd7c1f2b5f5006ed2f9c879b
RivineShoheiMatsuda <shohei.matsuda@rivine.jp>
2022-11-21 16:03:38 +0900

    タイポ修正

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { useState, useRef } from "react";
 import TodoList from "./TodoList";
 import { v4 as uuidv4 } from "uuid";
 function App() {
+  
   const [todos, setTodos] = useState([
     {id: 1, name: "Todo1", completed: false},
   ]);
 
   const todoNameRef = useRef();
 
-  const handoleAddTodo = () => {
+  const handleAddTodo = () => {
     // タスクを追加する
     const name = todoNameRef.current.value;
     if(name === "") return;
@@ -25,7 +26,7 @@ function App() {
     setTodos(newTodos);
   };
 
-  const handoleClear = () => {
+  const handleClear = () => {
     const newTodos = todos.filter((todo) => !todo.completed);
     setTodos(newTodos);
   };
@@ -34,8 +35,8 @@ function App() {
     <div>
       <TodoList todos={todos} toggleTodo={toggleTodo} />
       <input type="text" ref={todoNameRef} />
-      <button onClick={handoleAddTodo}>タスクを追加</button>
-      <button onClick={handoleClear}>完了したタスクの削除</button>
+      <button onClick={handleAddTodo}>タスクを追加</button>
+      <button onClick={handleClear}>完了したタスクの削除</button>
       <div>残りのタスク：{todos.filter((todo) => !todo.completed).length}</div>
     </div>
   );
